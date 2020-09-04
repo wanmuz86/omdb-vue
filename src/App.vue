eb7306bedb527641f0da0d9ca2481e6c61a0e0d6
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-09-04 21:07:10 +0800

    add item

--- src/App.vue
+++ src/App.vue
@@ -7,31 +7,21 @@
   </div>
   <div>
     <h2>Results</h2>
-    <div id="section">
-      <div>
+    <div id="section" >
+      <div v-for="film in films" :key="film.imdbID" v-on:click="selectFilm ">
         <img src="">
-        <h3>Tenet</h3>
-        <p>2020 - Film</p>
+        <h3>{{film.Title}}</h3>
+        <p>{{film.Year}} - {{film.Type}}</p>
       </div> 
-      <div>
-        <img src="">
-        <h3>Tenet</h3>
-        <p>2020 - Film</p>
-      </div> 
-      <div>
-        <img src="">
-        <h3>Tenet</h3>
-        <p>2020 - Film</p>
-      </div> 
+      
     </div>
   </div>
-  <div id="more-info">
+  <div id="more-info" v-if="selectedFilm">
     <img src="">
-    <p>Tenet</p>
-    <p>2020</p>
+    <p>{{selectedFilm.Title}}</p>
+    <p>{{selectedFilm.Year}}</p>
     <p>Robert Pattingson, Washington </p>
     <p>plot...</p>
-    
   </div>
   </div>
 
@@ -41,6 +31,46 @@
 
 export default {
   name: 'App',
+  methods:{
+selectFilm:function(event){
+
+  this.selectedFilm = {
+      "Title": "Tenet",
+      "Year": "2020",
+      "imdbID": "tt6723592",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_SX300.jpg"
+}
+}
+  },
+  data() {
+    return {
+      "selectedFilm":null,
+      "films" : [
+      {
+      "Title": "Tenet",
+      "Year": "2020",
+      "imdbID": "tt6723592",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_SX300.jpg"
+},
+{
+"Title": "Roma te tenet et amoR",
+"Year": "2011",
+"imdbID": "tt2069901",
+"Type": "movie",
+"Poster": "N/A"
+},
+{
+"Title": "Dark Tenet",
+"Year": "2014",
+"imdbID": "tt2243233",
+"Type": "movie",
+"Poster": "https://m.media-amazon.com/images/M/MV5BNzdlNmIzNjMtYzMyNS00NjkwLWJkNjAtNjhlNzIyZTIyZDUwXkEyXkFqcGdeQXVyNDk3OTU1ODc@._V1_SX300.jpg"
+},
+]
+    }
+  }
   
 }
 </script>
