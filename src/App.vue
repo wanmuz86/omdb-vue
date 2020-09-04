57bb099749ce9c80c3128f6e6bf27216570adb0f
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-09-04 21:58:42 +0800

    add item

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,59 @@
 <template>
   <div id="app">
     <div>
-  <h1>My Movie app</h1>
-  <input type="text" name="search"/>
-  <button>Search</button>
-  </div>
-  <div>
-    <h2>Results</h2>
-    <div id="section" >
-      <div v-for="film in films" :key="film.imdbID" v-on:click="selectFilm ">
-        <img v-bind:src="film.Poster">
-        <h3>{{film.Title}}</h3>
-        <p>{{film.Year}} - {{film.Type}}</p>
-      </div> 
-      
+      <h1>My Movie app</h1>
+      <input type="text" name="search" v-model="searchTerm"/>
+      <button v-on:click="buttonPressed">Search</button>
     </div>
-  </div>
-  <div id="more-info" v-if="selectedFilm">
-    <img src="">
-    <p>{{selectedFilm.Title}}</p>
-    <p>{{selectedFilm.Year}}</p>
-    <p>Robert Pattingson, Washington </p>
-    <p>plot...</p>
-  </div>
+    <div>
+      <h2>Results</h2>
+      <div id="section" >
+        <div v-for="film in films" :key="film.imdbID" v-on:click="selectFilm(film)">
+          <img v-bind:src="film.Poster">
+          <h3>{{film.Title}}</h3>
+          <p>{{film.Year}} - {{film.Type}}</p>
+        </div> 
+
+      </div>
+    </div>
+    <div id="more-info" v-if="selectedFilm">
+      <img src="">
+      <p>{{selectedFilm.Title}}</p>
+      <p>{{selectedFilm.Year}}</p>
+      <p>Robert Pattingson, Washington </p>
+      <p>plot...</p>
+    </div>
   </div>
 
 </template>
 
 <script>
 
-export default {
-  name: 'App',
-  methods:{
-selectFilm:function(event){
-console.log(event)
-  this.selectedFilm = {
-      "Title": "Tenet",
-      "Year": "2020",
-      "imdbID": "tt6723592",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_SX300.jpg"
-}
-}
-  },
-  data() {
-    return {
-      "selectedFilm":null,
-      "films" : [
-      {
-      "Title": "Tenet",
-      "Year": "2020",
-      "imdbID": "tt6723592",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_SX300.jpg"
-},
-{
-"Title": "Roma te tenet et amoR",
-"Year": "2011",
-"imdbID": "tt2069901",
-"Type": "movie",
-"Poster": "N/A"
-},
-{
-"Title": "Dark Tenet",
-"Year": "2014",
-"imdbID": "tt2243233",
-"Type": "movie",
-"Poster": "https://m.media-amazon.com/images/M/MV5BNzdlNmIzNjMtYzMyNS00NjkwLWJkNjAtNjhlNzIyZTIyZDUwXkEyXkFqcGdeQXVyNDk3OTU1ODc@._V1_SX300.jpg"
-},
-]
+  export default {
+    name: 'App',
+
+    methods:{
+      buttonPressed:function(){
+        fetch('http://www.omdbapi.com/?s='+this.searchTerm+'&apikey=87d10179') .then(response => response.json())
+        .then(data => 
+          this.films = data.Search
+          );
+      },
+
+      selectFilm:function(film){
+        this.selectedFilm = film
+      }
+    },
+    data() {
+      return {
+        "searchTerm":"",
+        "selectedFilm":null,
+        "films" : [
+        ]
+      }
     }
+
   }
-  
-}
 </script>
 
 <style>
